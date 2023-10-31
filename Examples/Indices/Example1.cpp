049b0700a4626aaf5e7882c2055188fc5884097e
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2023-10-31 20:46:40 +0300

    Add indices example

--- Examples/Indices/Example1.cpp
+++ Examples/Indices/Example1.cpp
@@ -1,4 +1,116 @@
-//1 Easy Difficulty
-//This only requires the change of a few variables!
-//Exercise 1. Draw a square using 4 vertices and 6 indices
-//Hint : create comments so you know which coordinate is which
+#include <glad.h>
+#include <glfw3.h>
+#include <iostream>
+#include <FragmentShader.h>
+#include <VertexShader.h>
+#include <Constants.h>
+
+int main()
+{
+	//1 Easy Difficulty
+	//This only requires the change of a few variables!
+	//Exercise 1. Draw a square using 4 vertices and 6 indices
+	//Hint : create comments so you know which coordinate is which
+
+	//Init GLFW
+	glfwInit();
+
+	//GLFW ctx settings
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Creating the window with glfw
+	GLFWwindow* window;// -> allocated on stack
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Window is null. Terminating" << std::endl;
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		-0.5f, 0.5f * float(sqrt(3)) / 6,0.0f,
+		0.5f , 0.5f * float(sqrt(3)) / 6,0.0f,
+	};
+
+	GLuint indices[] =
+	{
+		0 , 1 , 2,
+		2 , 1 , 3,
+	};
+
+	//Create vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Create fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//Create shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	//Link shader program
+	glLinkProgram(shaderProgram);
+
+	//Delete shader sources
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//Create VAO(Vertex Array Object)
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//Create VBO(Vertex Buffer Object) 
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Create EBO (Element Buffer Object)
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//Change color 
+		glClearColor(0.7f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//Swap front and back buffers
+		glfwSwapBuffers(window);
+
+		//Poll events so that it doesn't freeze
+		glfwPollEvents();
+	}
+
+	//Clean VAO, VBO and shaderProgram
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
+}
